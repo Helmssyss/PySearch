082faf001cb04d410acb4165a658e0e1af7d6da9
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2023-01-17 21:19:58 +0300

    Add files via upload

--- pysearch/google_search/GSearch.py
+++ pysearch/google_search/GSearch.py
@@ -1,50 +1,56 @@
 from ..util.constants import headers
-import threading
-import requests
 from bs4 import BeautifulSoup
 from typing import Optional
+import threading
+import requests
 
 class GoogleSearch:
+    __lock = threading.Lock()
+    __threads:list[threading.Thread] = []
     def __init__(self,query:Optional[str]=None,page:Optional[int]=None) -> None:
-        self.page = page
-        self.query = query
+        self.page:Optional[str] = page
+        self.query:Optional[str] = query
         self.__links:list[str] = []
-        self.__lock = threading.Lock()
 
     def __enter__(self):
         return self
     
     def __exit__(self,*args):
         self.__links.clear()
+        self.__threads.clear()
+    
+    def __delete__(self):
+        self.__links
+        self.__lock
+        self.__threads
     
     def __getCookie(self) -> dict:
-        with requests.Session() as session:
-            __result = session.get("https://www.google.com/",headers=headers)
-            __cookies = __result.cookies.get_dict()
-            return __cookies
+        __result = requests.get("https://www.google.com/",headers=headers)
+        __cookies = __result.cookies.get_dict()
+        return __cookies
     
     def __searchQuery(self,slot):
-        with requests.Session() as session:
-            params = {
-                "q" : self.query,
-                "start": slot
-            }
-            response = session.get("https://www.google.com/search",params=params,headers=headers,cookies=self.__getCookie())
-            soup = BeautifulSoup(response.content,"lxml")
-            for i in  soup.find_all("div",attrs={"class":"yuRUbf"}):
-                self.__lock.acquire()
-                self.__links.append(i.a["href"])
-                self.__lock.release()
+        params = {
+            "q" : self.query,
+            "start": slot
+        }
+        response = requests.get("https://www.google.com/search",params=params,headers=headers,cookies=self.__getCookie())
+        soup = BeautifulSoup(response.content,"lxml")
+        for i in  soup.find_all("div",attrs={"class":"yuRUbf"}):
+            self.__lock.acquire()
+            link = i.a["href"]
+            self.__links.append(link)
+            self.__lock.release()
+            
     @property
     def links(self) -> list[str]:
-        threads:list[threading.Thread] = []
         for p in range(0, self.page*10 if self.page == 1 else (self.page) * 10, 10):
             # self.__searchQuery(p)
             t = threading.Thread(target=self.__searchQuery,args=(p,))
             t.start()
-            threads.append(t)
+            self.__threads.append(t)
         
-        for thread in threads:
+        for thread in self.__threads:
             thread.join()
         
         return self.__links
